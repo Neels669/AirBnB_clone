e554c71aae5055b45c9bfc8a965b5ff416b0288a
Neelam <neelam_669@yahoo.com>
2023-10-14 10:11:51 -0700

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,32 @@
 """This module is the base class of all our models."""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """The class defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """ instantiation """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            for k, v in kwargs.items():
+                if k == 'created_at':
+                    self.created_at = datetime.strptime(
+                        v,
+                        '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        v,
+                        '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == '__class__':
+                    self.__class__.__name__ = v
+                else:
+                    setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Print object"""
@@ -29,7 +42,7 @@ class BaseModel:
     def to_dict(self):
         """"returns a dictionary containing all keys/values of instance"""
         my_dict = self.__dict__.copy()
-        my_dict['__class__'] = self.__class__.__name__
+        my_dict['__class__'] = (self.__class__.__name__)
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
