8ab594e07cecc8cba708cca7b203c8a5e7f2faac
Neelam <neelam_669@yahoo.com>
2023-10-15 01:29:35 -0700

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,15 +6,11 @@ from datetime import datetime
 
 class BaseModel:
     """The class defines all common attributes/methods for other classes"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """ instantiation """
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Print object"""
