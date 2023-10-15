ebd348e94050060afcb569d26efaaeb25f843596
Neelam <neelam_669@yahoo.com>
2023-10-15 03:14:00 -0700

    Storage

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,24 @@ class BaseModel:
     """The class defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """ instantiation """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            self.updated_at = datetime.strptime(self.updated_at,
-                                                "%Y-%m-%dT%H:%M:%S.%f")
-            self.created_at = datetime.strptime(self.created_at,
-                                                "%Y-%m-%dT%H:%M:%S.%f")
+            for k, v in kwargs.items():
+                if k == 'created_at':
+                    self.created_at = datetime.strptime(
+                        v,
+                        '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        v,
+                        '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == '__class__':
+                    self.__class__.__name__ = v
+                else:
+                    setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
