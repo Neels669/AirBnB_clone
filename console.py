e1171529640e4a7ecd19e52c583eaa26948447a1
Neelam <neelam_669@yahoo.com>
2023-10-17 06:55:33 -0700

    Console

--- console.py
+++ console.py
@@ -5,11 +5,17 @@ import sys
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """This class defines the cmd"""
-    classes = ["BaseModel", "User"]
+    classes = ["BaseModel", "User", "State",
+               "City", "Amenity", "Place", "Review"]
 
     prompt = "(hbnb) "
 
